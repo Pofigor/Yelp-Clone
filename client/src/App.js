8fa8fe71ec2cd607c50e378903a35833c75ec99d
Aslanbek <kaipaeff@gmail.com>
2022-12-15 00:41:36 +0300

    added MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React from 'react';
+// import Login from './Components/Login/Login';
+// import Info from './Components/Info/Info';
+// import { Container, Nav } from 'react-bootstrap';
+// import { Routes, Route } from 'react-router-dom'
+// import Context from './context';
+import Nav from './Components/Nav/Nav'
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='app'>
+      <header className='app-header'>
+        <Nav />
       </header>
     </div>
   );
---
+++ client/src/Components/Nav/Nav.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+// import Button from '@mui/material/Button';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+import styles from './Nav.module.css'
+import { Link } from 'react-router-dom';
+import Container from '@mui/material/Container';
+
+
+
+export default function ButtonAppBar() {
+  return (
+    <Container fixed>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar className={styles.nav}>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <div className={styles.leftDiv}>
+            <Link to='/' className={styles.homeBtn} variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Home
+            </Link>
+          </div>
+
+          <div className={styles.rightDiv}>
+            <Link to='/info' className={styles.infoBtn} color="inherit">Info</Link>
+            <Link to='/login' className={styles.loginBtn} color="inherit">Login</Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </Container>
+  );
+}
