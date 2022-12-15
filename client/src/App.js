b9c868efb06083f1f7596eb28f22d7957e3d0ed2
Aslanbek <kaipaeff@gmail.com>
2022-12-15 12:00:20 +0300

    newDay

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-// import Login from './Components/Login/Login';
-// import Info from './Components/Info/Info';
-// import { Container, Nav } from 'react-bootstrap';
-// import { Routes, Route } from 'react-router-dom'
-// import Context from './context';
+
 import Nav from './Components/Nav/Nav'
 
+import { Routes, Route } from 'react-router-dom'
+
+import Container from '@mui/material/Container';
 
 
 function App() {
-
-
   return (
     <div className='app'>
+
       <header className='app-header'>
         <Nav />
       </header>
+
+      <Container fixed>
+        <Routes>
+
+          <Route path='/' element={<img src='/img/1.JPG' alt='img' />} />
+          <Route path='/info' element={<img src='/img/2.jpg' alt='img' />} />
+          <Route path='/login' element={<img src='/img/3.jpg' alt='img' />} />
+        </Routes>
+
+      </Container>
     </div>
   );
 }
